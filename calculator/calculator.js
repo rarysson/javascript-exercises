881ff1111ffb972d58c927e61a37879147165ef6
Rarysson Guilherme <r_guilherme12@hotmail.com>
2019-11-19 23:45:56 -0300

    Solution of Calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add(a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum(array) {
+	const result = array.reduce(function(total, value) {
+		return total + value;
+	}, 0);
+
+	return result;
 }
 
-function multiply () {
-	
+function multiply(array) {
+	const result = array.reduce(function(total, value) {
+		return total * value;
+	}, 1);
+
+	return result;
 }
 
-function power() {
-	
+function power(base, exp) {
+	if (exp <= 1) {
+		return base;
+	}
+
+	return base * power(base, exp - 1);
 }
 
-function factorial() {
-	
+function factorial(number) {
+	if (number <= 1) {
+		return 1;
+	}
+
+	return number * factorial(number - 1);
 }
 
 module.exports = {
