7ecaa7a1342687cbebb44ed4d11d7dbd1f0dedae
Rarysson Guilherme <r_guilherme12@hotmail.com>
2019-11-20 16:27:29 -0300

    Solution of FindTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let oldest = people[0];
+    let date = new Date();
 
+    people.forEach(person => {
+        let currentPerson;
+        let nextPerson;
+
+        if (oldest.yearOfDeath === undefined) {
+            currentPerson = date.getFullYear() - oldest.yearOfBirth;
+        } else {
+            currentPerson = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        if (person.yearOfDeath === undefined) {
+            nextPerson = date.getFullYear() - person.yearOfDeath;
+        } else {
+            nextPerson = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (nextPerson > currentPerson) {
+            oldest = person;
+        }
+    });
+
+    return oldest;
 }
 
 module.exports = findTheOldest
