67c8c15a3f8dc1d3c0713668a0bb9bee4e2c16c8
Rarysson Guilherme <r_guilherme12@hotmail.com>
2019-11-20 10:22:26 -0300

    Solution of Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(text) {
+	const plainText = text.replace(/[^a-zA-Z]/g, "").toLowerCase();
+	const reversedText = plainText.split("").reverse().join("");
+	
+	return plainText === reversedText;
 }
 
 module.exports = palindromes
