e9245629718a859e3fe9757e917d3c3bcfead355
Rarysson Guilherme <r_guilherme12@hotmail.com>
2020-02-23 16:18:16 -0300

    Solution of pig_latin

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Solutions that I've done:
 * getTheTitles
 * findTheOldest
 * snakeCase
+* pig_latin
 
 ## About
 These are a series of javascript exercises intended to be used alongside the curriculum at 'The Odin Project'  They start very simply, but get more involved as you progress through them.
--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,39 @@
-function translate() {
-	// body...
+function translate(text) {
+	isVowel = (letter) => {
+		return letter == "a" ||
+		letter == "e" ||
+		letter == "i" ||
+		letter == "o" ||
+		letter == "u";
+	};
+
+	let final_text = "";
+	const array = text.split(" ");
+
+	array.forEach(word => {
+        let initial_letter = word[0];
+        let index = 1;
+
+        if (isVowel(initial_letter)) {
+            final_text += word + "ay";
+		} else {
+            while (!isVowel(word[index])) {
+                index++;
+			}
+
+            if (word[index - 1] == "q" && word[index] == "u") {
+                index++;
+			}
+
+            final_text += word.slice(index, word.length) + word.slice(0, index) + "ay";
+        }
+
+        final_text += " ";
+	});
+
+    final_text = final_text.slice(0, final_text.length - 1);
+
+    return final_text;
 }
 
 
